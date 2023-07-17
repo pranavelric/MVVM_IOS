26f0dd10407c9c644e08da0ef1ee16d2926c8020
Pranav Choudhary <pranavchoudhary500@gmail.com>
2023-07-17 19:49:42 +0530

    HTTP utility added

--- MvvmIOS/Services/HttpUtility.swift
+++ MvvmIOS/Services/HttpUtility.swift
@@ -7,3 +7,44 @@
 
 import Foundation
 
+struct HttpUtility{
+    
+    func getApiData<T: Decodable>(requestUrl:URL, resultType: T.Type,completionHandler: @escaping(_ result:T?)->Void){
+        URLSession.shared.dataTask(with: requestUrl){(responseData, httpResponse,error) in
+            
+            if(error==nil && responseData != nil && responseData?.count != 0){
+                let decoder = JSONDecoder()
+                do{
+                    let result =  try decoder.decode(T.self, from: responseData!)
+                    completionHandler(result)
+                }
+                catch let error{
+                    debugPrint("error while decoding \(error.localizedDescription)")
+                }
+            }
+        }.resume()
+    }
+    
+    
+    func postApiData<T: Decodable>(requestUrl:URL, requestBody: Data, resultType: T.Type,completionHandler: @escaping(_ result:T?)->Void){
+        
+        var urlRequest =  URLRequest(url: requestUrl)
+        urlRequest.httpMethod = "post"
+        urlRequest.httpBody = requestBody
+        urlRequest.addValue("application/json", forHTTPHeaderField: "content-type")
+        URLSession.shared.dataTask(with: urlRequest){(responseData, httpResponse,error) in
+            
+            if( responseData != nil && responseData?.count != 0){
+                let decoder = JSONDecoder()
+                do{
+                    let result =  try decoder.decode(T.self, from: responseData!)
+                    completionHandler(result)
+                }
+                catch let error{
+                    debugPrint("error while decoding \(error.localizedDescription)")
+                }
+            }
+        }.resume()
+        
+    }
+}
